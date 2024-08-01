759c0e85432f8a35a2b71a9d75b0beb573c0883b
Madhavanesh <madhavanesh.clar@gmail.com>
2024-08-01 15:21:55 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,190 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    clar_technologies_logo,
+    kct_logo,
+    ai_blog,
+    anilask_logo
+} from "../assets/icons";
+
+export const skills = [
+    // {
+    //     imageUrl: css,
+    //     name: "CSS",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    // {
+    //     imageUrl: motion,
+    //     name: "Motion",
+    //     type: "Animation",
+    // },
+    // {
+    //     imageUrl: mui,
+    //     name: "Material-UI",
+    //     type: "Frontend",
+    // },
+    // {
+    //     imageUrl: nextjs,
+    //     name: "Next.js",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: redux,
+    //     name: "Redux",
+    //     type: "State Management",
+    // },
+    // {
+    //     imageUrl: sass,
+    //     name: "Sass",
+    //     type: "Frontend",
+    // },
+    // {
+    //     imageUrl: tailwindcss,
+    //     name: "Tailwind CSS",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Research Co-ordinator",
+        company_name: "Department Association of ISE",
+        icon: kct_logo,
+        iconBg: "#accbe1",
+        date: "Jan 2023 - Feb 2024",
+        points: [
+            "Oversee research projects from inception to completion, ensuring timely execution and adherence to budgets.",
+            "Foster research collaborations among faculty and students, providing guidance and support for student researchers.",
+            "Identify funding opportunities, prepare grant proposals, and manage awarded grants effectively.",
+            "Ensure all research activities align with ethical guidelines and regulatory standards.",
+        ],
+    },
+    {
+        title: "Software Engineer Intern",
+        company_name: "Clar Technologies",
+        icon: clar_technologies_logo,
+        iconBg: "#fbc3bc",
+        date: "Jul 2023 - Jun 2024",
+        points: [
+            "Software Developer Engineer with a year of experience in blockchain-based supply chain traceability.",
+            "Proficient in full-stack development using React, TypeScript, and Payload CMS.",
+            "Expertise in decentralized key management and data privacy.",
+            "Skilled in programming languages: C, Java, Python, JavaScript, TypeScript, Go, and Solidity.",
+            "Passionate about leveraging blockchain for innovative supply chain solutions.",
+        ],
+    },
+    {
+        title: "Software Development Engineer",
+        company_name: "Clar Technologies",
+        icon: clar_technologies_logo,
+        iconBg: "#fbc3bc",
+        date: "Jul 2024 - Present",
+        points: [
+            "Skilled Software Developer Engineer with a year of experience in building blockchain-based supply chain traceability solutions. Proficient in full-stack development using React, TypeScript, and Payload CMS. Strong foundation in decentralized key management and data privacy. Possessing a versatile skill set encompassing C, Java, Python, JavaScript, TypeScript, Go, and Solidity.",
+            "Passionate about harnessing blockchain technology to drive innovation in supply chain operations. Committed to creating user-centric applications that improve efficiency and transparency. Seeking opportunities to contribute to cutting-edge projects that shape the future of the industry."
+        ],
+    }
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Madhavanesh',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'www.linkedin.com/in/madhavanesh-kannan-8b7192203',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: ai_blog,
+        theme: 'btn-back-red',
+        name: 'AI Blog Generator',
+        description: 'Developed a Python-based application utilizing the Django framework to efficiently convert engaging YouTube videos into informative blog posts.',
+        link: 'https://github.com/Madhavanesh/AI-Blog-Generator',
+    },
+    {
+        iconUrl: anilask_logo,
+        theme: 'btn-back-green',
+        name: 'AniLask anime website',
+        description: 'Created a fundamental anime streaming website using Django for backend operations and HTML for frontend development, providing a basic platform for users to enjoy their favorite anime shows.',
+        link: 'https://github.com/Madhavanesh/Anime-Website',
+    }
+];
